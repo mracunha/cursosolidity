240297b1e94c512e05087af7742c1ea68ca3fb5d
mracunha <145159309+mracunha@users.noreply.github.com>
2023-09-19 18:08:13 -0300

    Add files via upload

---
+++ mrac_token_nft_721_exercicio.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+// Metadados: https://mracunha.github.io/nft-mrac/
+//Contrato Sepolia: 0x036ee2E14126C75A4d50F286b8A0D3245Ab3D0E2
+
+contract TokenMracTeste is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("TokenMRAC", "TMRAC") {
+        _tokenIdCounter.increment();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://mracunha.github.io/nft-mrac/";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return string.concat(super.tokenURI(tokenId), ".json");
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
